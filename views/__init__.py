06eb50533d0e592f93b900175e15b8650e35bf05
Joe Albrecht <joedevpro@gmail.com>
2024-06-07 10:21:46 -0500

    feaure update

--- views/__init__.py
+++ views/__init__.py
@@ -3,4 +3,3 @@ from .post import new_post, list_posts, get_user_posts, get_post_by_id
 from .category import new_category, list_categories, update_category, retrieve_category, delete_category
 from .tag import list_tags, retrieve_tag, create_tag, update_tag, delete_tag
 from .comment import get_comments_by_post_id, new_comment
-
